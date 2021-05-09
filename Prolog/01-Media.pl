03371b9d6fdd0664ec942e0dddd800bc45307dd9
perdizio <43557422+perdizio@users.noreply.github.com>
2021-05-09 19:53:55 -0300

    Update and rename exemplo1 to 01-Media.pl

--- Prolog/01-Media.pl
+++ Prolog/01-Media.pl
@@ -1,3 +1,7 @@
+%Ele ler três valores, responde na tela qual é o maior entre eles e
+%fornece a média aritmética também na tela.
+
+
 maior(X,Y,M):- X > Y, M = X | M = Y.
 maior(X,Y,Z,MM):- maior(X,Y,M), M > Z, MM = M | MM = Z.
 media(X,Y,Z,M):- M is (X + Y + Z) / 3.
